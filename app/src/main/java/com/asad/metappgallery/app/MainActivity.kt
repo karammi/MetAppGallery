aa0df5efc1fd8c77fdbdbc8559e11a79ba9340d2
asadkarami <asad.karammi@gmail.com>
2023-04-14 08:44:08 +0330

    chore: Moved MainActivity to app module

--- app/src/main/java/com/asad/metappgallery/app/MainActivity.kt
+++ app/src/main/java/com/asad/metappgallery/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.asad.metappgallery
+package com.asad.metappgallery.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,11 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.asad.metappgallery.ui.theme.MetAppGalleryTheme
+import com.asad.metappgallery.app.theme.MetAppGalleryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,24 +17,10 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
                 ) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MetAppGalleryTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/asad/metappgallery/app/theme/Color.kt
+++ app/src/main/java/com/asad/metappgallery/app/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.asad.metappgallery.ui.theme
+package com.asad.metappgallery.app.theme
 
 import androidx.compose.ui.graphics.Color
 
