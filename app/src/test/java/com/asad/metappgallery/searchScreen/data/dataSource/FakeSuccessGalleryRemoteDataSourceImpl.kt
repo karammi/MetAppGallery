aa0d24f7a3fc938d55cc69b186ff63294f2ff340
asadkarami <asad.karammi@gmail.com>
2023-04-23 13:46:47 +0330

    chore: Fixed unit test issues

--- app/src/test/java/com/asad/metappgallery/searchScreen/data/dataSource/FakeSuccessGalleryRemoteDataSourceImpl.kt
+++ app/src/test/java/com/asad/metappgallery/searchScreen/data/dataSource/FakeSuccessGalleryRemoteDataSourceImpl.kt
@@ -1,16 +1,23 @@
 package com.asad.metappgallery.searchScreen.data.dataSource
 
 import com.asad.metappgallery.core.data.DataResult
+import com.asad.metappgallery.searchScreen.data.model.DepartmentResponse
 import com.asad.metappgallery.searchScreen.data.model.GalleryResponse
 
 class FakeSuccessGalleryRemoteDataSourceImpl : GalleryRemoteDataSource {
-
-    override suspend fun fetchList(query: String): DataResult<GalleryResponse> {
+    override suspend fun fetchList(
+        query: String,
+        isHighlight: Boolean?,
+    ): DataResult<GalleryResponse> {
         return DataResult.Success(
             value = GalleryResponse(
                 total = 10,
                 objectIDs = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
             ),
         )
+    }
+
+    override suspend fun fetchDepartments(): DataResult<DepartmentResponse> {
+        TODO("Not yet implemented")
     }
 }
