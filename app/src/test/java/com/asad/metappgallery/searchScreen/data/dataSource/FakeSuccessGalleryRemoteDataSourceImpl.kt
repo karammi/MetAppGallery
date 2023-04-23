e800b65f70b712d1f28145e1c376c2d1bf5bc647
asadkarami <asad.karammi@gmail.com>
2023-04-23 11:52:15 +0330

    chore: Refactored GallerySearchViewModel unit tests

---
+++ app/src/test/java/com/asad/metappgallery/searchScreen/data/dataSource/FakeSuccessGalleryRemoteDataSourceImpl.kt
@@ -0,0 +1,16 @@
+package com.asad.metappgallery.searchScreen.data.dataSource
+
+import com.asad.metappgallery.core.data.DataResult
+import com.asad.metappgallery.searchScreen.data.model.GalleryResponse
+
+class FakeSuccessGalleryRemoteDataSourceImpl : GalleryRemoteDataSource {
+
+    override suspend fun fetchList(query: String): DataResult<GalleryResponse> {
+        return DataResult.Success(
+            value = GalleryResponse(
+                total = 10,
+                objectIDs = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+            ),
+        )
+    }
+}
