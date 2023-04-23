885a2d2ab7949fdb961fabfd696d2bdab3fa7264
asadkarami <asad.karammi@gmail.com>
2023-04-23 23:42:01 +0330

    chore: Refactored searchScreen, clear focus after searching result is ready

--- app/src/main/java/com/asad/metappgallery/searchScreen/data/adapter/GallerySearchConstant.kt
+++ app/src/main/java/com/asad/metappgallery/searchScreen/data/adapter/GallerySearchConstant.kt
@@ -1,6 +1,5 @@
 package com.asad.metappgallery.searchScreen.data.adapter
 
-import android.util.Log
 import com.asad.metappgallery.core.util.Emoji
 
 private const val TAG = "GalleryResponseConstant"
@@ -9,12 +8,8 @@ object GallerySearchConstant {
     const val Total: String = "total"
     const val ObjectIDs: String = "objectIDs"
 
-    val GalleryCollectionApi: (String, Boolean?) -> String = { queryString, isHighlight ->
-        val temp =
-            "https://collectionapi.metmuseum.org/public/collection/v1/search?${isHighlight.let { "isHighlight=$it&" }}q=$queryString"
-
-        Log.d(TAG, "url: $temp")
-        temp
+    val GalleryCollectionApi: (String, Boolean?) -> String = { queryString, _ ->
+        "https://collectionapi.metmuseum.org/public/collection/v1/search?q=$queryString"
     }
 
     const val DepartmentCollectionApi =
